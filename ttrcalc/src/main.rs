6680dac19cd1f59d645745b169a79cc384c0541a
Your Name <you@example.com>
2021-02-01 01:46:33 +0100

    WIP: Add crossbeam, fix formatting

--- ttrcalc/src/main.rs
+++ ttrcalc/src/main.rs
@@ -1,13 +1,17 @@
-mod game;
+mod board;
+mod gamestate;
 mod parser;
 mod route;
 
-use game::Game;
+use board::Board;
+use crossbeam::deque::{Injector, Stealer, Worker}; // https://docs.rs/crossbeam/0.8.0/crossbeam/deque/index.html
+use gamestate::GameState;
 use parser::parse_routes;
 use route::Route;
+use std::iter;
 
 fn main() {
-    let mut game = Game::new(20);
+    let mut game = Board::new(20);
     parse_routes("london_tracks.csv", &mut game);
 
     let routes: Vec<String> = game
@@ -15,4 +19,26 @@ fn main() {
         .map(|route| game.fmt_route(route))
         .collect();
     println!("{:?}", routes);
+
+    let gamestate = GameState::new(&game);
+    let gamestate = gamestate.new_state_with_route(1);
+    println!("GameState: {:?}", gamestate);
+}
+
+fn find_task<T>(local: &Worker<T>, global: &Injector<T>, stealers: &[Stealer<T>]) -> Option<T> {
+    // Pop a task from the local queue, if not empty.
+    local.pop().or_else(|| {
+        // Otherwise, we need to look for a task elsewhere.
+        iter::repeat_with(|| {
+            // Try stealing a batch of tasks from the global queue.
+            global
+                .steal_batch_and_pop(local)
+                // Or try stealing a task from one of the other threads.
+                .or_else(|| stealers.iter().map(|s| s.steal()).collect())
+        })
+        // Loop while no task was stolen and any steal operation needs to be retried.
+        .find(|s| !s.is_retry())
+        // Extract the stolen task, if there is one.
+        .and_then(|s| s.success())
+    })
 }
--- ttrcalc/src/parser.rs
+++ ttrcalc/src/parser.rs
@@ -1,11 +1,9 @@
-use std::env;
 use std::fs;
 
-use crate::game::Game;
+use crate::board::Board;
 use crate::route::Route;
 
 fn parse_csv_line(input: &str) -> Vec<&str> {
-    println!("Parse {}", input);
     input.split(",").collect()
 }
 
@@ -24,7 +22,7 @@ fn parse_csv_file<'a>(fname: &str, buffer: &'a mut String) -> Vec<Vec<&'a str>> 
     return parsed;
 }
 
-pub fn parse_routes(fname: &str, game: &mut Game) {
+pub fn parse_routes(fname: &str, board: &mut Board) {
     let mut buffer = String::new();
     let csv = parse_csv_file(fname, &mut buffer);
     let csv = csv.iter().skip(1); // skip header
@@ -32,19 +30,20 @@ pub fn parse_routes(fname: &str, game: &mut Game) {
         if line.len() < 2 {
             continue;
         }
-        let city1_id = game.add_get_city(String::from(line[0]));
-        let city2_id = game.add_get_city(String::from(line[1]));
+        let city1_id = board.add_get_city(String::from(line[0]));
+        let city2_id = board.add_get_city(String::from(line[1]));
         let length = line[2].parse::<usize>();
+        /*
         println!(
             "{:?}={},{:?}={},{:?} ==> {:?}",
             line[0], city1_id, line[1], city2_id, line[2], length
-        );
+        );*/
         let length = length.expect(format!("Cannot parse number \"{}\"", line[2]).as_str());
         let route = Route::new(
             city1_id,
             city2_id,
             crate::route::unknown_track_of_length(length),
         );
-        game.add_route(route);
+        board.add_route(route);
     }
 }
